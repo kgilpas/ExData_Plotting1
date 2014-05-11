1e9d0000ddc3f330f3382742b1dd441e2e013fd0
Kenneth Gil-Pasquel <kennethgilpasquel@gmail.com>
2014-05-11 18:40:36 -0400

    Add plot1.R

---
+++ plot1.R
@@ -0,0 +1,29 @@
+# Set my working directory
+setwd("C:/Users/ENVY/Desktop")
+
+# Read in data by unzipping file and loading via read.table
+temp <- tempfile() 
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp) 
+data <- read.table(unz(temp, "household_power_consumption.txt"),sep=";",header=TRUE,na.strings="?") 
+unlink(temp)
+
+
+#Subset data
+pwrConsum <- data[as.character(data$Date) %in% c("1/2/2007", "2/2/2007"),]
+
+# Fix dates
+pwrConsum$Timestamp <- paste(pwrConsum$Date,pwrConsum$Time, sep=' ')
+pwrConsum$Timestamp <- strptime(pwrConsum$Timestamp,format='%d/%m/%Y %H:%M:%S')
+
+# Plot 1
+
+png(file="./plot1.png", width=480,height=480, bg = "transparent")
+
+hist(
+  pwrConsum$Global_active_power,
+  xlab = "Global Active Power (kilowatts)",
+  main ="Global Active Power",
+  col = "red"
+  )
+
+dev.off()
